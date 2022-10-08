12053cd05fd0219a0aa2b5e0c33c0cc32410e304
amir sadough <85782806+amirsadough94@users.noreply.github.com>
2022-10-08 20:31:15 +0330

    Create myZip.py

---
+++ myZip.py
@@ -0,0 +1,22 @@
+l1 = [1, 2, 3, 4, 5]
+l2 = ['a', 'b', 'c']
+
+
+def myZip(*input_data):
+    seq = [list(s) for s in input_data]
+    res = []
+    while all(seq):
+        res.append(tuple(s.pop(0) for s in seq))
+    return res
+
+
+def myPad(*input_data, **kargs):
+    pad = kargs.get("pad", None)
+    seq = [list(s) for s in input_data]
+    res = []
+    while any(seq):
+        res.append(tuple(s.pop(0) if s else pad for s in seq))
+    return res
+
+
+print(myPad(l1, l2, pad='?'))
